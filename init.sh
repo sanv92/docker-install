d6b4f59912d5044034965173617712bc0b21b233
SanderV1992 <sander.vergeles@gmail.com>
2016-09-25 01:55:11 +0300

    fix

--- init.sh
+++ init.sh
@@ -32,3 +32,5 @@ sudo usermod -aG docker $USER
 # new # GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"
 sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/g' /etc/default/grub
 sudo update-grub
+
+sudo service docker restart
